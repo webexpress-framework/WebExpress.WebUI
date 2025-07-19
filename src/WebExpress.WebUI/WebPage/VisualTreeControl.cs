583b3029acd53b8bf16e6a0d45b692641adf6575
ReneSchwarzer <rene_schwarzer@hotmail.de>
2025-07-19 23:25:10 +0200

    remove: jquery

--- src/WebExpress.WebUI/WebPage/VisualTreeControl.cs
+++ src/WebExpress.WebUI/WebPage/VisualTreeControl.cs
@@ -139,7 +139,6 @@ namespace WebExpress.WebUI.WebPage
             _cssLinks.Add(RouteEndpoint.Combine(baseUri, "css/webexpress.webui.toolpanel.css"));
             _cssLinks.Add(RouteEndpoint.Combine(baseUri, "css/webexpress.webui.tree.css"));
 
-            _headerScriptLinks.Add(RouteEndpoint.Combine(baseUri, "js/jquery-3.7.1.min.js"));
             _headerScriptLinks.Add(RouteEndpoint.Combine(baseUri, "js/popper.min.js"));
             _headerScriptLinks.Add(RouteEndpoint.Combine(baseUri, "js/bootstrap.min.js"));
             _headerScriptLinks.Add(RouteEndpoint.Combine(baseUri, "js/webexpress.webui.js"));
