3cf9e21c566ef8a25e14c314b1ab375419262118
Rene Schwarzer <rene_schwarzer@hotmail.de>
2025-09-22 22:14:50 +0200

    feat: add toast fragments, general improvements and minor bugs

--- src/WebExpress.WebUI.Test/WebControl/UnitTestControlPanelToast.cs
+++ src/WebExpress.WebUI.Test/WebControl/UnitTestControlPanelToast.cs
@@ -14,8 +14,8 @@ namespace WebExpress.WebUI.Test.WebControl
         /// Tests the id property of the toast control.
         /// </summary>
         [Theory]
-        [InlineData(null, @"<div></div>")]
-        [InlineData("id", @"<div id=""id""></div>")]
+        [InlineData(null, @"<div class=""alert"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData("id", @"<div id=""id"" class=""alert"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
         public void Id(string id, string expected)
         {
             // preconditions
@@ -36,15 +36,16 @@ namespace WebExpress.WebUI.Test.WebControl
         /// Tests the background color property of the toast control.
         /// </summary>
         [Theory]
-        [InlineData(TypeColorBackground.Default, @"<div></div>")]
-        [InlineData(TypeColorBackground.Primary, @"<div class=""bg-primary""></div>")]
-        [InlineData(TypeColorBackground.Secondary, @"<div class=""bg-secondary""></div>")]
-        [InlineData(TypeColorBackground.Warning, @"<div class=""bg-warning""></div>")]
-        [InlineData(TypeColorBackground.Danger, @"<div class=""bg-danger""></div>")]
-        [InlineData(TypeColorBackground.Dark, @"<div class=""bg-dark""></div>")]
-        [InlineData(TypeColorBackground.Light, @"<div class=""bg-light""></div>")]
-        [InlineData(TypeColorBackground.Transparent, @"<div class=""bg-transparent""></div>")]
-        public void BackgroundColor(TypeColorBackground backgroundColor, string expected)
+        [InlineData(TypeColorBackgroundAlert.Default, @"<div class=""alert"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeColorBackgroundAlert.Primary, @"<div class=""alert bg-primary"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeColorBackgroundAlert.Secondary, @"<div class=""alert bg-secondary"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeColorBackgroundAlert.Info, @"<div class=""alert alert-info"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeColorBackgroundAlert.Warning, @"<div class=""alert alert-warning"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeColorBackgroundAlert.Danger, @"<div class=""alert alert-danger"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeColorBackgroundAlert.Dark, @"<div class=""alert alert-dark"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeColorBackgroundAlert.Light, @"<div class=""alert alert-light"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeColorBackgroundAlert.Transparent, @"<div class=""alert bg-transparent"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        public void BackgroundColor(TypeColorBackgroundAlert backgroundColor, string expected)
         {
             // preconditions
             var componentHub = UnitTestControlFixture.CreateAndRegisterComponentHubMock();
@@ -52,7 +53,7 @@ namespace WebExpress.WebUI.Test.WebControl
             var visualTree = new VisualTreeControl(componentHub, context.PageContext);
             var control = new ControlPanelToast()
             {
-                BackgroundColor = new PropertyColorBackground(backgroundColor)
+                BackgroundColor = new PropertyColorBackgroundAlert(backgroundColor)
             };
 
             // test execution
@@ -65,11 +66,11 @@ namespace WebExpress.WebUI.Test.WebControl
         /// Tests the direction property of the toast control.
         /// </summary>
         [Theory]
-        [InlineData(TypeDirection.Default, @"<div></div>")]
-        [InlineData(TypeDirection.Vertical, @"<div class=""flex-column""></div>")]
-        [InlineData(TypeDirection.VerticalReverse, @"<div class=""flex-column-reverse""></div>")]
-        [InlineData(TypeDirection.Horizontal, @"<div class=""flex-row""></div>")]
-        [InlineData(TypeDirection.HorizontalReverse, @"<div class=""flex-row-reverse""></div>")]
+        [InlineData(TypeDirection.Default, @"<div class=""alert"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeDirection.Vertical, @"<div class=""alert flex-column"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeDirection.VerticalReverse, @"<div class=""alert flex-column-reverse"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeDirection.Horizontal, @"<div class=""alert flex-row"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeDirection.HorizontalReverse, @"<div class=""alert flex-row-reverse"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
         public void Direction(TypeDirection direction, string expected)
         {
             // preconditions
@@ -91,9 +92,9 @@ namespace WebExpress.WebUI.Test.WebControl
         /// Tests the fluid property of the toast control.
         /// </summary>
         [Theory]
-        [InlineData(TypePanelContainer.None, @"<div></div>")]
-        [InlineData(TypePanelContainer.Default, @"<div class=""container""></div>")]
-        [InlineData(TypePanelContainer.Fluid, @"<div class=""container-fluid""></div>")]
+        [InlineData(TypePanelContainer.None, @"<div class=""alert"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypePanelContainer.Default, @"<div class=""alert container"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypePanelContainer.Fluid, @"<div class=""alert container-fluid"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
         public void Fluid(TypePanelContainer fluid, string expected)
         {
             // preconditions
@@ -115,9 +116,9 @@ namespace WebExpress.WebUI.Test.WebControl
         /// Tests the theme property of the panel control.
         /// </summary>
         [Theory]
-        [InlineData(TypeTheme.None, @"<div></div>")]
-        [InlineData(TypeTheme.Light, @"<div data-bs-theme=""light""></div>")]
-        [InlineData(TypeTheme.Dark, @"<div data-bs-theme=""dark""></div>")]
+        [InlineData(TypeTheme.None, @"<div class=""alert"" role=""alert""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeTheme.Light, @"<div class=""alert"" role=""alert"" data-bs-theme=""light""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(TypeTheme.Dark, @"<div class=""alert"" role=""alert"" data-bs-theme=""dark""><div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
         public void Theme(TypeTheme theme, string expected)
         {
             // preconditions
@@ -139,9 +140,9 @@ namespace WebExpress.WebUI.Test.WebControl
         /// Tests the add function of the toast control.
         /// </summary>
         [Theory]
-        [InlineData(typeof(ControlText), @"<div><div></div></div>")]
-        [InlineData(typeof(ControlLink), @"<div><a class=""link""></a></div>")]
-        [InlineData(typeof(ControlImage), @"<div><img></div>")]
+        [InlineData(typeof(ControlText), @"<div class=""alert"" role=""alert""><div><div></div></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(typeof(ControlLink), @"<div class=""alert"" role=""alert""><div><a class=""link""></a></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
+        [InlineData(typeof(ControlImage), @"<div class=""alert"" role=""alert""><div><img></div><button class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""close""></button></div>")]
         public void Add(Type child, string expected)
         {
             // preconditions
--- src/WebExpress.WebUI/WebControl/ControlPanelToast.cs
+++ src/WebExpress.WebUI/WebControl/ControlPanelToast.cs
@@ -1,4 +1,7 @@
-﻿namespace WebExpress.WebUI.WebControl
+﻿using WebExpress.WebCore.WebHtml;
+using WebExpress.WebUI.WebPage;
+
+namespace WebExpress.WebUI.WebControl
 {
     /// <summary>
     /// Represents a toast notification control panel that can contain 
@@ -7,6 +10,38 @@
     public class ControlPanelToast : ControlPanel
     {
         /// <summary>
+        /// Returns or set the background color.
+        /// </summary>
+        public new PropertyColorBackgroundAlert BackgroundColor
+        {
+            get => (PropertyColorBackgroundAlert)GetPropertyObject();
+            set => SetProperty(value, () => value?.ToClass(), () => value?.ToStyle());
+        }
+
+        /// <summary>
+        /// Returns or sets whether the control can be closed.
+        /// </summary>
+        public TypeDismissibleAlert Dismissible
+        {
+            get => (TypeDismissibleAlert)GetProperty(TypeDismissibleAlert.Dismissible);
+            set => SetProperty(value, () => value.ToClass());
+        }
+
+        /// <summary>
+        /// Returns or sets whether the fader effect should be used.
+        /// </summary>
+        public TypeFade Fade
+        {
+            get => (TypeFade)GetProperty(TypeFade.None);
+            set => SetProperty(value, () => value.ToClass());
+        }
+
+        /// <summary>
+        /// Returns or sets the headline.
+        /// </summary>
+        public string Head { get; set; }
+
+        /// <summary>
         /// Initializes a new instance of the class.
         /// </summary>
         /// <param name="id">The id of the control.</param>
@@ -14,6 +49,40 @@
         public ControlPanelToast(string id = null, params ControlAlert[] controls)
             : base(id, controls)
         {
+        }
+
+        /// <summary>
+        /// Converts the control to an HTML representation.
+        /// </summary>
+        /// <param name="renderContext">The context in which the control is rendered.</param>
+        /// <param name="visualTree">The visual tree representing the control's structure.</param>
+        /// <returns>An HTML node representing the rendered control.</returns>
+        public override IHtmlNode Render(IRenderControlContext renderContext, IVisualTreeControl visualTree)
+        {
+            var head = new HtmlElementTextSemanticsStrong
+            (
+                new HtmlText(Head),
+                new HtmlNbsp()
+            );
+
+            var button = new HtmlElementFieldButton()
+            {
+                Class = "btn-close"
+            };
+            button.AddUserAttribute("data-bs-dismiss", "alert");
+            button.AddUserAttribute("aria-label", "close");
+
+            return new HtmlElementTextContentDiv()
+            {
+                Id = Id,
+                Class = Css.Concatenate("alert", GetClasses()),
+                Style = GetStyles(),
+                Role = "alert",
+                DataTheme = Theme.ToValue()
+            }
+                .Add(!string.IsNullOrWhiteSpace(Head) ? head : null)
+                .Add(new ControlPanel().Add(Content).Render(renderContext, visualTree))
+                .Add(Dismissible != TypeDismissibleAlert.None ? button : null);
         }
     }
 }
