d10cbac408f1376849262a6bf7b80230bc894f59
Rene Schwarzer <rene_schwarzer@hotmail.de>
2025-10-05 09:19:59 +0200

    feat: improved sidebar control and minor bugs

---
+++ src/WebExpress.WebUI/WebFragment/IFragmentControlDropdownItem.cs
@@ -0,0 +1,14 @@
+﻿using WebExpress.WebCore.WebFragment;
+
+namespace WebExpress.WebUI.WebFragment
+{
+    /// <summary>
+    /// Represents an item in the dropdown of a fragment control.
+    /// </summary>
+    /// <remarks>This interface defines the contract for dropdown items that can be used within a fragment
+    /// control. Implementations of this interface may provide specific functionality or behavior for dropdown
+    /// actions.</remarks>
+    public interface IFragmentControlDropdownItem : IFragmentBase
+    {
+    }
+}
