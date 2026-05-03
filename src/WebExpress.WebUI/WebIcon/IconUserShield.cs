159eed6977d830e109804e541111ddb096db2d9e
Rene Schwarzer <rene_schwarzer@hotmail.de>
2026-05-03 15:03:16 +0200

    feat: general improvements and minor bugs

--- src/WebExpress.WebUI/WebIcon/IconUserShield.cs
+++ src/WebExpress.WebUI/WebIcon/IconUserShield.cs
@@ -1,3 +1,5 @@
+using WebExpress.WebCore.WebIcon;
+
 namespace WebExpress.WebUI.WebIcon
 {
     /// <summary>
@@ -6,8 +8,29 @@ namespace WebExpress.WebUI.WebIcon
     public class IconUserShield : Icon
     {
         /// <summary>
-        /// Returns the CSS class associated with the icon.
+        /// Initializes a new instance of the class.
         /// </summary>
-        public override string Class => "fas fa-user-shield";
+        public IconUserShield()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the class using the
+        /// specified theme.
+        /// </summary>
+        /// <param name="theme">The theme to use when rendering the icon.</param>
+        public IconUserShield(TypeIconTheme theme)
+            : base(theme)
+        {
+        }
+
+        /// <summary>
+        /// Returns the CSS class associated with the icon. In the
+        /// <see cref="TypeIconTheme.Light"/> theme the lightweight SVG variant is rendered;
+        /// otherwise the FontAwesome glyph is used.
+        /// </summary>
+        public override string Class => Theme == TypeIconTheme.Light
+            ? "wx-icon-light wx-icon-light-user-shield"
+            : "fas fa-user-shield";
     }
 }
